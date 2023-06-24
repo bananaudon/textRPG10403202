48741e3578a65341029423973b05c88a02201dc2
bananaudon <hibi533king@gmail.com>
2023-06-25 05:35:58 +0900

    delete itemID,itemType

--- RPG/src/textRPG10403202/items/item.java
+++ RPG/src/textRPG10403202/items/item.java
@@ -1,18 +1,12 @@
 package textRPG10403202.items;
 import textRPG10403202.RPGCharacter;
 public abstract class item {
-    int itemID;
-    int itemType;
     String itemName;
     int remaining;
-
     void setItemStatus(int I, int IT, String N, int R) {
-        itemID = I;
-        itemType = IT;
         itemName = N;
         remaining = R;
     }
-
     public abstract void executionItem(RPGCharacter c);
     public String getItemName(){
         return this.itemName;
@@ -24,8 +18,6 @@ public abstract class item {
         remaining--;
     }
     item() {
-        itemID = 0;
-        itemType = 0;
         itemName = "NONE";
         remaining = -1;
     }
--- RPG/src/textRPG10403202/items/item_void.java
+++ RPG/src/textRPG10403202/items/item_void.java
@@ -2,8 +2,6 @@ package textRPG10403202.items;
 import textRPG10403202.RPGCharacter;
 public class item_void extends item {
     public item_void() {
-        itemID = 0;
-        itemType = 0;
         itemName = "NONE";
         remaining = -1;
     }
