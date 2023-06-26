4c858016459b5fe3726dc5d99a22385f7114cfbe
bananaudon <hibi533king@gmail.com>
2023-06-27 01:29:38 +0900

    gameManagerメソッド分割

--- RPG/src/textRPG10403202/gameManager.java
+++ RPG/src/textRPG10403202/gameManager.java
@@ -5,15 +5,20 @@ public class gameManager {
         return this.activeGameState;
     }
     void setStatus(gameState newState){
-        if(newState == gameState.EXPLORE||newState == gameState.GAME_OVER){
-            this.activeGameState = newState;
-            return;
-        }
-        if(this.activeGameState == gameState.EXPLORE){
-            this.activeGameState = newState;
+        if(checkStatus(newState)){
+            activeGameState = newState;
             return;
         }
         System.out.println("エラーメッセージ:" + activeGameState + "から" + newState + "へは移行できません、システムを終了します");
         System.exit(0);
+    }
+    boolean checkStatus(gameState newState){
+        if(newState == gameState.EXPLORE||newState == gameState.GAME_OVER){
+            return true;
+        }
+        if(this.activeGameState == gameState.EXPLORE){
+            return true;
+        }
+        return false;
     }
 }
