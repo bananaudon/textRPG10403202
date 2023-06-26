a61abe069d8caf9827f13dc61cd9d04d2839f7e2
bananaudon <hibi533king@gmail.com>
2023-06-26 21:52:46 +0900

    test

---
+++ RPG/src/textRPG10403202/items/Heal.java
@@ -0,0 +1,29 @@
+package textRPG10403202.items;
+
+public abstract class Heal{
+    private String itemName;
+    private int durability,durabilityLossPerUse;
+    protected int fixedHeal;
+    Heal(String n,int d){
+        this.durability = d;
+        this.itemName = n;
+    }
+    public String getItemName(){
+        return this.itemName;
+    }
+    public void Rename(String name){
+        this.itemName = name;
+    }
+    public int getdurability(){
+        return this.durability;
+    }
+    public void decrementdurability(){
+        this.durability -= durabilityLossPerUse;
+    }
+    public boolean isBreak(){
+        if(this.durability <= 0){
+            return true;
+        }
+        return false;
+    }
+}
---
+++ RPG/src/textRPG10403202/items/Heal_Low.java
@@ -0,0 +1,26 @@
+package textRPG10403202.items;
+
+import textRPG10403202.RPGCharacter;
+import textRPG10403202.RPG2;
+public class Heal_Low extends Heal implements item{
+    public Heal_Low(int fixedHeal) {
+        this(fixedHeal,"低位ポーション",5);
+    }
+    public Heal_Low(int fixedHeal,String name) {
+        this(fixedHeal,name,5);
+    }
+    public Heal_Low(int fixedHeal,String name,int durability) {
+        super(name,durability);
+        this.fixedHeal = fixedHeal;
+    }
+    public static void applyEffect(RPGCharacter c,Heal_Low HL) {
+        int percentageHeal = (c.maxHP / 10);
+        c.heal(HL.fixedHeal + percentageHeal);
+        RPG2.logWrite((HL.fixedHeal + percentageHeal) + "回復した");
+    }
+    public void use(RPGCharacter target){
+        RPG2.logWrite(this.getItemName() + "を使った");
+        applyEffect(target,this);
+        decrementdurability();
+    }
+}
