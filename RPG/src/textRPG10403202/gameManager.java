c43dcb58eb4ccae1fad38ab2fd22fd4f61e325f3
bananaudon <hibi533king@gmail.com>
2023-06-27 01:13:43 +0900

    setStateメソッド追加

--- RPG/src/textRPG10403202/gameManager.java
+++ RPG/src/textRPG10403202/gameManager.java
@@ -1,4 +1,19 @@
 package textRPG10403202;
 public class gameManager {
     private gameState activeGameState = gameState.EXPLORE;
+    gameState getNowGameState(){
+        return this.activeGameState;
+    }
+    void setStatus(gameState newState){
+        if(newState == gameState.EXPLORE||newState == gameState.GAME_OVER){
+            this.activeGameState = newState;
+            return;
+        }
+        if(this.activeGameState == gameState.EXPLORE){
+            this.activeGameState = newState;
+            return;
+        }
+        System.out.println("エラーメッセージ:" + activeGameState + "から" + newState + "へは移行できません、システムを終了します");
+        System.exit(0);
+    }
 }
