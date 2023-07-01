5e4c9554390e18bd3d530202b07c3c575c463855
bananaudon <hibi533king@gmail.com>
2023-07-01 17:45:57 +0900

    GameManager スペースを開けた

--- RPG/src/textRPG10403202/gameManager.java
+++ RPG/src/textRPG10403202/gameManager.java
@@ -1,9 +1,12 @@
 package textRPG10403202;
 public class GameManager {
+    
     private GameState activeGameState = GameState.EXPLORE;
+
     GameState getActiveGameState(){
         return this.activeGameState;
     }
+
     void setStatus(GameState newState){
         if(checkStatus(newState)){
             activeGameState = newState;
@@ -12,6 +15,7 @@ public class GameManager {
         System.out.println("エラーメッセージ:" + activeGameState + "から" + newState + "へは移行できません、システムを終了します");
         System.exit(0);
     }
+
     private boolean checkStatus(GameState newState){
         if(newState == GameState.EXPLORE||newState == GameState.GAME_OVER){
             return true;
@@ -21,6 +25,7 @@ public class GameManager {
         }
         return false;
     }
+
     void nextStatus(){
         switch(activeGameState){
             case EXPLORE -> {
