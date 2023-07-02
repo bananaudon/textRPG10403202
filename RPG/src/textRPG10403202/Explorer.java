8551df655865e14ca6bd7dac8e7b1d07f0a475e7
bananaudon <hibi533king@gmail.com>
2023-07-02 22:39:40 +0900

    RPGCharacter　Explorer アイテム関連のインスタンスをRPGCharacterからExplorerに移動

--- RPG/src/textRPG10403202/Explorer.java
+++ RPG/src/textRPG10403202/Explorer.java
@@ -1,18 +1,51 @@
 package textRPG10403202;
+
+import textRPG10403202.items.Item;
+import textRPG10403202.items.ItemBag;
+import textRPG10403202.items.ItemBagJFrame;
+import textRPG10403202.items.ItemInfo;
+
 public class Explorer extends RPGCharacter {
     int nextLv, EXP, steps, luck;
-
+    private ItemBag characterHave;
+    private ItemBagJFrame characterHaveJFrame;
+    private RPG2 mainClass;
     Explorer(int level, int maxHP, int currentHP, int power, RPG2 setMainClass,int nL, int E, int s, int lu){
         //characterとして必要な情報
-        super(level,maxHP,currentHP,power,setMainClass);
+        super(level,maxHP,currentHP,power);
 
         //主人公として必要な情報
+        this.mainClass = setMainClass;
         this.nextLv = nL;
         this.EXP = E;
         this.steps = s;
         this.luck = lu;
+        this.characterHave = new ItemBag(this);
+        this.characterHaveJFrame = new ItemBagJFrame(characterHave);
     }
 
+    public void useHealItem(){
+        this.characterHave.useHealItem();
+        mainClass.nextGameState();
+    }
+
+    public void setHeal(int bagNumber){
+        this.characterHave.setHealItem(bagNumber);
+        mainClass.nextGameState();
+    }
+
+    public void gotItem(Item item){
+        this.characterHave.addItem(item);
+    }
+
+    public void openItemBag(){
+        this.characterHaveJFrame.openItemBag();
+    }
+
+    public ItemInfo getHealItemInfo(){
+        return this.characterHave.getHealItemInfo();
+    }
+    
     @Override
     void attack(RPGCharacter c) {
         System.out.println(this.name + "は" + c.name + "を攻撃した");
--- RPG/src/textRPG10403202/RPGCharacter.java
+++ RPG/src/textRPG10403202/RPGCharacter.java
@@ -1,9 +1,5 @@
 package textRPG10403202;
 
-import textRPG10403202.items.Item;
-import textRPG10403202.items.ItemBag;
-import textRPG10403202.items.ItemBagJFrame;
-import textRPG10403202.items.ItemInfo;
 public abstract class RPGCharacter {
     public String name;
     
@@ -12,41 +8,13 @@ public abstract class RPGCharacter {
     public int maxHP;
     public int nowHP;
     public int Pow;
-    private RPG2 mainClass;
-    private ItemBag characterHave;
-    private ItemBagJFrame characterHaveJFrame;
-    RPGCharacter(int level, int maxHP, int currentHP, int power, RPG2 setMainClass) {
+    RPGCharacter(int level, int maxHP, int currentHP, int power) {
         this.lv = level;
         this.maxHP = maxHP;
         this.nowHP = currentHP;
         this.Pow = power;
-        this.mainClass = setMainClass;
-        this.characterHave = new ItemBag(this);
-        this.characterHaveJFrame = new ItemBagJFrame(characterHave);
     }
 
-    public void useHealItem(){
-        this.characterHave.useHealItem();
-        mainClass.nextGameState();
-    }
-
-    public void setHeal(int bagNumber){
-        this.characterHave.setHealItem(bagNumber);
-        mainClass.nextGameState();
-    }
-
-    public void gotItem(Item item){
-        this.characterHave.addItem(item);
-    }
-
-    public void openItemBag(){
-        this.characterHaveJFrame.openItemBag();
-    }
-
-    public ItemInfo getHealItemInfo(){
-        return this.characterHave.getHealItemInfo();
-    }
-    
     public void randomStatus(int minLv, int minMaxHP, int minNowHP, int minPow, int LRange, int MHRange, int NHRange,
             int PRange) {
         lv = (int) (Math.random() * (LRange + 1)) + minLv;
